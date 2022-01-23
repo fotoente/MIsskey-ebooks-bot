95c5b1a0ecbe88accc27267bc74b4ebece95d8c3
shibao <shibao@bubbletea.dev>
2022-01-23 15:34:06 -0500

    strip whitespace

--- rdbot.py
+++ rdbot.py
@@ -22,17 +22,17 @@ token=config.get("misskey","token")
 
 class MyBot(commands.Bot):
     text_model = None #Holds the markov object, so it won't be recreated everytime
-    
+
     def __init__(self):
         super().__init__()
         #self.text_model = read_posts()
-        #print(datetime.now().strftime('%Y-%m-%d %H:%M:%S')+" Posts initialized!")            
-        
+        #print(datetime.now().strftime('%Y-%m-%d %H:%M:%S')+" Posts initialized!")
+
     @tasks.loop(3600)
     async def loop_1h(self):
         if (bot.text_model is not None): #Just a check to see that there is a object stored
             await bot.client.note.send(content=create_post(bot.text_model))
-    
+
     @tasks.loop(43200)
     async def loop_12h(self):
         bot.text_model = read_posts()
@@ -44,8 +44,8 @@ class MyBot(commands.Bot):
         self.loop_12h.start()  #Launching renew posts every 12 hours
         self.loop_1h.start() #
         print(datetime.now().strftime('%Y-%m-%d %H:%M:%S')+" Roboduck Bot started!")
-        
-        
+
+
     async def on_mention(self, note: Note):
         text=""
         if (not note.author.is_bot):
@@ -54,21 +54,20 @@ class MyBot(commands.Bot):
                 text = "@" + note.author.name + " " #Building the reply on same instance
             else:
                 text = "@" + note.author.name + "@" + note.author.host + " " #Building the reply on foreign instance
-            
+
             if (bot.text_model is not None):
                 markov = create_post(bot.text_model)
             else:
-                markov = "Error loading markov chain object!" 
-            
+                markov = "Error loading markov chain object!"
+
             if markov is not None: # better: if item is not None
                 text+=markov
-            else:        
-                text+="Error in markov chain sentence creation: Couldn't calculate sentence!\n\n☹ Please try again! " 
-            
+            else:
+                text+="Error in markov chain sentence creation: Couldn't calculate sentence!\n\n☹ Please try again! "
+
             await note.reply(content=text) #Reply to a note
 
 
 if __name__ == "__main__":
     bot = MyBot()
     asyncio.run(bot.start(uri, token, timeout=600))
-   
